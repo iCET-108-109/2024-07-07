4ec0e3cad52782c3ee134e34be46c6ed73e83e6b
icet-sharadha <icet.sharadha@gmail.com>
2024-07-07 09:59:56 +0530

    map

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ fetch("https://restcountries.com/v3.1/all")
     data.forEach(element => {
 
         tableBody+=`<tr>
-                        <td><h1>${element.name.common}</h1> <br> ${element.name.official}</td>
+                        <td><h1>${element.name.common}</h1> <br>
+                        
+                        Official Name : ${element.name.official}
+                        <p>Region : ${element.region} </p>
+                        <p>Population : ${element.population} </p>
+                        <a class="btn btn-primary" href="${element.maps.googleMaps}">Go To Map</a>
+                        </td>
                         <td><img src="${element.flags.png}" alt=""></td>
                     </tr>`
         console.log(element.name.common);
